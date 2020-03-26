2926db57cf5968dee9056f5ba43dfe35571bc74a
tal nc <talnisan8@gmail.com>
2020-03-26 12:34:07 +0200

    simplified normalize function

--- src/corona_hakab_model/consts.py
+++ src/corona_hakab_model/consts.py
@@ -13,7 +13,7 @@ from util import dist
 
 class Consts(NamedTuple):
     # simulation parameters
-    population_size = 10_000
+    population_size = 100_000
     total_steps = 400
     initial_infected_count = 20
 
