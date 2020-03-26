fixed small bug in markovic matrix generation. changed average_time_of_infectious to be average time in each state, returning a dict of state to how long you stay in this state on average. improved normalization

src/corona_hakab_model/consts.py
@@ -1,19 +1,19 @@
 from functools import lru_cache
 from itertools import count
-from typing import NamedTuple
+from typing import NamedTuple, Dict, List
 
 import numpy as np
 from scipy.stats import rv_discrete
 
 from medical_state import ImmuneState, InfectableState, InfectiousState
-from medical_state_machine import MedicalStateMachine
+from medical_state_machine import MedicalStateMachine, MedicalState
 from state_machine import StochasticState, TerminalState
-from util import dist
+from util import dist, upper_bound
 
 
 class Consts(NamedTuple):
     # simulation parameters
-    population_size = 100_000
+    population_size = 10_000
     total_steps = 400
     initial_infected_count = 20
 
@@ -35,28 +35,48 @@ class Consts(NamedTuple):
         7
     )  # todo maybe the program should juts print a question mark,  we'll see how the researchers like that!
 
-    def average_infecting_days(self):
+    def average_time_in_each_state(self):
+        """
+        calculate the average time an infected agent spends in any of the states.
+        uses markov chain to do the calculations
+        note that it doesnt work well for terminal states
+        :return: dict of states: int, representing the average time an agent would be in a given state
+        """
         TOL = 1e-6
         m = self.medical_state_machine()
-        M, terminal_rows, entry_columns = m.markovian
+        M, terminal_states, transfer_states, entry_columns = m.markovian
         z = len(M)
 
         p = entry_columns[m.state_upon_infection]
         terminal_mask = np.zeros(z, bool)
-        terminal_mask[list(terminal_rows.values())] = True
+        terminal_mask[list(terminal_states.values())] = True
 
-        ret = 0.0
-        prev_v = 0
+        states_duration = {state: 0 for state in m.states}
+        states_duration[m.state_upon_infection] = 1
+
+        index_to_state: Dict[int: MedicalState] = {}
+        for state, index in terminal_states.items():
+            index_to_state[index] = state
+        for state, dict in transfer_states.items():
+            first_index = dict[0]
+            last_index = dict[max(dict.keys())] + upper_bound(state.durations[-1])
+            for index in range(first_index, last_index):
+                index_to_state[index] = state
+
+        prev_v = 0.0
         for time in count(1):
             p = M @ p
             v = np.sum(p, where=terminal_mask)
             d = v - prev_v
-            ret += d * time
             prev_v = v
+
+            for i, prob in enumerate(p):
+                states_duration[index_to_state[i]] += prob
+
             # run at least as many times as the node number to ensure we reached all terminal nodes
             if time > z and d < TOL:
                 break
-        return ret
+        return states_duration
 
     # average probability for transmitions:
     silent_to_asymptomatic_probability = 0.2
@@ -226,4 +246,4 @@ class Consts(NamedTuple):
 
 if __name__ == "__main__":
     c = Consts()
-    print(c.average_infecting_days())
+    print(c.average_time_in_each_state())

src/corona_hakab_model/state_machine.py
@@ -168,7 +168,7 @@ class StateMachine(Generic[T]):
         """
 
         # each initial transfer state gets its own index
-        transfer_starts: Dict[StochasticState, Dict[int, int]] = {}
+        transfer_states: Dict[StochasticState, Dict[int, int]] = {}
 
         # terminal_states each get their own index
         terminal_states: Dict[TerminalState, int] = {}
@@ -183,7 +183,7 @@ class StateMachine(Generic[T]):
                 for i, dur in enumerate(s.durations):
                     state_dict[i] = next_index
                     next_index += upper_bound(dur)
-                transfer_starts[s] = state_dict
+                transfer_states[s] = state_dict
             else:
                 raise TypeError
 
@@ -194,7 +194,7 @@ class StateMachine(Generic[T]):
             ec[i] = 1
             entry_columns[t_state] = ec
 
-        for s_state, s_dict in transfer_starts.items():
+        for s_state, s_dict in transfer_states.items():
             ec = np.zeros(next_index, float)
             for transfer_index, node_index in s_dict.items():
                 p_of_transfer = s_state.prob_specific(transfer_index)
@@ -205,13 +205,13 @@ class StateMachine(Generic[T]):
             # todo if this is slow we can do it faster
             ret[:, terminal_index] = entry_columns[t_state]
 
-        for s_state, sub_dict in transfer_starts.items():
+        for s_state, sub_dict in transfer_states.items():
             for transfer_index, transfer_start in sub_dict.items():
                 dur = s_state.durations[transfer_index]
                 dest = s_state.destinations[transfer_index]
                 p_passed = 1
-                for i in range(upper_bound(dur) + 1):
-                    p_exit_unbiased = dur.pmf(i)
+                for i in range(upper_bound(dur)):
+                    p_exit_unbiased = dur.pmf(i + 1)
                     p_exit = p_exit_unbiased / p_passed
                     p_passed *= 1 - p_exit
                     ret[:, transfer_start + i] = p_exit * entry_columns[dest]
@@ -221,6 +221,6 @@ class StateMachine(Generic[T]):
         for col in range(next_index):
             assert np.sum(ret[:, col]) == 1
 
-        return ret, terminal_states, entry_columns
+        return ret, terminal_states, transfer_states, entry_columns
 
     # todo function to draw a pretty graph