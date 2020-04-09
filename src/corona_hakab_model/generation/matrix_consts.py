a5157bc5be19364b19d3219d0c5267c8a94bf040
Shemesh <mor.m.shemesh@gmail.com>
2020-04-10 01:30:33 +0300

    Updated MatrixConsts and CirclesConsts to match Consts rework

--- src/corona_hakab_model/generation/matrix_consts.py
+++ src/corona_hakab_model/generation/matrix_consts.py
@@ -1,60 +1,64 @@
-from collections import namedtuple
 from generation.connection_types import ConnectionTypes
+from typing import Dict, NamedTuple, Tuple
 
 """
 Overview:
 
-We have default_parameters - it is our template ans as the name suggests, holds the default values
-Using that template, we create MatrixConsts, a named tuple.
-the Consts class inherits from MatrixConsts the fields, and adds the methods.
-This is how we preserve the efficiency of a NamedTuple but also get dynamic values
+MatrixConsts class is a named tuple consts for the Matrix creation stage of the SimulationData generation.
+it may either be made using default params, or by loading parameters from a file.
 Usage:
-1. Create a default consts object - consts = MatrixConsts()
-2. Load a parameters file - consts = MatrixConsts.from_file(path)
+1. Create a default consts object - consts = Consts()
+2. Load a parameters file - consts = Consts.from_file(path)
 """
-default_parameters = {
-    "connection_type_to_connection_strength": {
+
+
+class MatrixConsts(NamedTuple):
+    # In order to create instances of the class the fields must be typed
+
+    # Attributes and default values
+
+    # Strengths of each connection type - how likely are people to infect one another in that circle
+    connection_type_to_connection_strength: Dict = {
         ConnectionTypes.Family: 3,
         ConnectionTypes.Work: 0.66,
         ConnectionTypes.School: 1,
         ConnectionTypes.Other: 0.23,
-    },
-    "daily_connections_amount_by_connection_type": {
+    }
+    daily_connections_amount_by_connection_type: Dict = {
         ConnectionTypes.School: 6,
         ConnectionTypes.Work: 5.6,
         ConnectionTypes.Other: 0.4,
-    },
-    "weekly_connections_amount_by_connection_type": {
+    }
+    weekly_connections_amount_by_connection_type: Dict = {
         ConnectionTypes.School: 12.6,
         ConnectionTypes.Work: 12.6,
         ConnectionTypes.Other: 6,
-    },
-    "use_parasymbolic_matrix": True,
-    "clustering_switching_point": (50,),
-    "community_triad_probability": (1,),
-}
+    }
 
-MatrixParameters = namedtuple(
-    "MatrixParameters",
-    sorted(default_parameters),
-    defaults=[default_parameters[key] for key in sorted(default_parameters)],
-)
+    clustering_switching_point: Tuple = (50,)
+    community_triad_probability: Tuple = (1,)
+    use_parasymbolic_matrix: bool = True
 
-
-class MatrixConsts(MatrixParameters):
-    __slots__ = ()
-
-    @staticmethod
-    def from_file(param_path):
+    @classmethod
+    def from_file(cls, param_path):
         """
         Load parameters from file and return MatrixConsts object with those values.
 
         No need to sanitize the eval'd data as we disabled __builtins__ and only passed specific functions
-        Documentation about what is allowed and not allowed can be found at the top of this page.
         """
         with open(param_path, "rt") as read_file:
             data = read_file.read()
 
-        parameters = eval(data, {"__builtins__": None, "ConnectionTypes": ConnectionTypes})
+        # expressions to evaluate
+        expressions = {
+            "__builtins__": None,
+            "ConnectionTypes": ConnectionTypes,
+        }
 
-        return MatrixConsts(**parameters)
+        parameters = eval(data, expressions)
+
+        return cls(**parameters)
+
+    # overriding hash and eq to allow caching while using un-hashable attributes
+    __hash__ = object.__hash__
+    __eq__ = object.__eq__
