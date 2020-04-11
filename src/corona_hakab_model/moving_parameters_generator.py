51bb16c858566ed78a6ef6a17056e2ffd065c00e
RoyIsland <roy1.island@gmail.com>
2020-04-11 18:08:55 +0300

    added doc to functions

--- src/corona_hakab_model/moving_parameters_generator.py
+++ src/corona_hakab_model/moving_parameters_generator.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import pathlib
 from typing import List
 
-MovingParameter = namedtuple("MovingParameter", "param_name start_range end_range step_size", )
+MovingParameter = namedtuple("MovingParameter", "param_name start_range end_range step_size")
 
 
 class MovingParametersGenerator:
@@ -24,16 +24,26 @@ class MovingParametersGenerator:
 
     @classmethod
     def replace_parameter_value(cls, params_str, param_name, param_value):
+        """
+        Replace value of parameter name in parameters string and return replaced string.
+        """
+
         first_str = f"\"{param_name}\": "
         second_str = ","
 
-        first_value_pos = params_str.find(first_str)
-        second_value_pos = params_str.find(second_str, first_value_pos)
-        return params_str[:first_value_pos + len(first_str)] + str(param_value) + params_str[second_value_pos:]
+        first_str_index = params_str.find(first_str)
+        second_str_index = params_str.find(second_str, first_str_index)
+        return params_str[:first_str_index + len(first_str)] + str(param_value) + params_str[second_str_index:]
 
     @classmethod
     def write_generated_parameters_to_file(cls, generated_params, moving_param_name, moving_param_value,
                                            output_folder_path):
+        """
+        Write value of generated_params to output file.
+        File path is output folder path + moving parameter name + moving parameter value.
+        Create directories of file path if not exists.
+        """
+
         write_folder_path = f"{output_folder_path}/{moving_param_name}"
 
         pathlib.Path(write_folder_path).mkdir(parents=True, exist_ok=True)
@@ -42,8 +52,8 @@ class MovingParametersGenerator:
 
 
 if __name__ == "__main__":
-    input_file_path = "Parameters/circles_parameters_example.py"
-    output_folder_path = "../../output/generated_parameters/circles_parameters"
+    circles_file_path = "Parameters/circles_parameters_example.py"
+    circles_output_folder_path = "../../output/generated_parameters/circles_parameters"
 
     population_size_param = MovingParameter(param_name="population_size",
                                             start_range=20_000,
@@ -55,4 +65,5 @@ if __name__ == "__main__":
                                                step_size=1)
     input_moving_params = [population_size_param, geo_circles_amount_param]
 
-    MovingParametersGenerator.generate_moving_parameters(input_file_path, input_moving_params, output_folder_path)
+    MovingParametersGenerator.generate_moving_parameters(circles_file_path, input_moving_params,
+                                                         circles_output_folder_path)
