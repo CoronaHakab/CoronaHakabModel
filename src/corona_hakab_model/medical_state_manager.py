a9320505e46f2e456d784f900b2f777333cc2c01
bgmoshe <barakgross5@gmail.com>
2020-04-12 16:40:35 +0300

    Issue 161: Added to MedicalStateManager extra optional parameter for step(): * update_only_medical_state - in order to update the medical state w/o using manager and not changing the whole god damn architecture, we added this flag.

--- src/corona_hakab_model/medical_state_manager.py
+++ src/corona_hakab_model/medical_state_manager.py
@@ -15,7 +15,14 @@ class MedicalStateManager:
         self.medical_state_machine = medical_state_machine
         self.pending_transfers = PendingTransfers()
 
-    def step(self, new_sick: List[Agent]):
+    def step(self, new_sick: List[Agent], update_only_medical_state: bool = False):
+        """
+
+        :param new_sick: List of new agents that got sick
+        :param update_only_medical_state: Defaults to False. If True, update agent.medical_state directly.
+                                          If False it also updates agent.manager.
+        :return:
+        """
         # all the new sick agents are leaving their previous step
         changed_state_leaving = defaultdict(list)
         # agents which are going to enter the new state
@@ -25,7 +32,10 @@ class MedicalStateManager:
         # all the new sick are going to get to the next state
         for agent in new_sick:
             changed_state_leaving[agent.medical_state].append(agent)
-            agent.set_medical_state_no_inform(self.medical_state_machine.get_state_upon_infection(agent))
+            if update_only_medical_state:
+                agent.medical_state = self.medical_state_machine.get_state_upon_infection(agent)
+            else:
+                agent.set_medical_state_no_inform(self.medical_state_machine.get_state_upon_infection(agent))
             changed_state_introduced[agent.medical_state].append(agent)
 
         # saves this number for supervising
@@ -33,8 +43,10 @@ class MedicalStateManager:
 
         moved = self.pending_transfers.advance()
         for (agent, destination, origin, _) in moved:
-            agent.set_medical_state_no_inform(destination)
-
+            if update_only_medical_state:
+                agent.medical_state = destination
+            else:
+                agent.set_medical_state_no_inform(destination)
             changed_state_introduced[destination].append(agent)
             changed_state_leaving[origin].append(agent)
 
