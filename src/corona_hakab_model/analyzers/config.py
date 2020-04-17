67f4910d5c91a6b82a3275a0c2010c29e7991877
NoamShapira <32097606+NoamShapira@users.noreply.github.com>
2020-04-17 14:09:09 +0300

    Analyzer/population analysis (#252)
    * initial commit
    
    * a code to demonstrate the problem in the wotk guid
    
    * plotting the relevant graphs
    
    * generlization of plotting population
    add example to example script
    exported simulation output files to config for latter change
    
    * fixes after PR
    changed file names to more informative names

---
+++ src/corona_hakab_model/analyzers/config.py
@@ -0,0 +1,5 @@
+from project_structure import OUTPUT_FOLDER
+from pathlib import Path
+
+TIME_OUTPUT_PATH = Path(__file__).parent / "../../output"
+POPULATION_OUTPUT_PATH = OUTPUT_FOLDER
--- src/corona_hakab_model/analyzers/example_script.py
+++ src/corona_hakab_model/analyzers/example_script.py
@@ -1,4 +1,5 @@
-from corona_hakab_model.analyzers.simulation_analysis import *
+from analyzers.agents_sickness_analysis import load_population_data_to_dfs, plot_sick_per_work_size
+from corona_hakab_model.analyzers.multi_simulation_analysis import *
 from project_structure import OUTPUT_FOLDER
 import os
 
@@ -31,3 +32,8 @@ plot_parameter_propagation_aggregated(parameter_names=[parameter_name],
 # you can also plot all the parameters time propagation
 plot_parameter_propagation_aggregated(mean_df=results_df_dict["mean"], std_df=results_df_dict["std"])
 
+
+# plot social circles sickness analysis
+initial_sick_data_df, final_sick_data_df, all_circles_df = load_population_data_to_dfs()
+plot_sick_per_work_size(final_sick_data_df, all_circles_df)
+plot_sick_per_work_size(initial_sick_data_df, all_circles_df)
