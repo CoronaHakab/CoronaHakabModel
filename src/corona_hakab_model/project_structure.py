534352e9c09cd26db6250d98a19973bf523e38d5
RoyIsland <roy1.island@gmail.com>
2020-04-20 10:34:40 +0300

    added output folder datetime

--- src/corona_hakab_model/project_structure.py
+++ src/corona_hakab_model/project_structure.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from datetime import datetime
 
 MODEL_FOLDER = Path(__file__).parent.parent
 print('MODEL_FOLDER: {}'.format(MODEL_FOLDER))
-OUTPUT_FOLDER = MODEL_FOLDER.parent / "output"
+OUTPUT_FOLDER = MODEL_FOLDER.parent / "output" / (datetime.now().strftime("%Y%m%d-%H%M%S"))
 SIM_OUTPUT_FOLDER = OUTPUT_FOLDER / "sim_records"
