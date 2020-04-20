80d9d5fc4858ef915adf4be31476a9db5e4e538c
rlfreiman <rlfreiman@gmail.com>
2020-04-20 20:38:25 +0300

    added synagogues to types and consts

--- src/corona_hakab_model/generation/connection_types.py
+++ src/corona_hakab_model/generation/connection_types.py
@@ -8,19 +8,20 @@ class ConnectionTypes(IntEnum):
     Kindergarten = 1
     School = 2
     Family = 3
-    Other = 4
+    Synagogue = 4
+    Other = 5
 
 
 # used for circles generation
-In_Zone_types = [ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Family]
+In_Zone_types = [ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Family, ConnectionTypes.Synagogue]
 Multi_Zone_types = [ConnectionTypes.Work]
 Whole_Population_types = [ConnectionTypes.Other]
 Non_Random_Age_Types = [ConnectionTypes.Family, ConnectionTypes.School]
 Education_Types = [ConnectionTypes.School, ConnectionTypes.Kindergarten]
 
-With_Random_Connections = [ConnectionTypes.Work, ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Other]
+With_Random_Connections = [ConnectionTypes.Work, ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Synagogue, ConnectionTypes.Other]
 
 # used for matrices generations
 Connect_To_All_types = [ConnectionTypes.Family]
-Random_Clustered_types = [ConnectionTypes.Work, ConnectionTypes.School, ConnectionTypes.Kindergarten]
+Random_Clustered_types = [ConnectionTypes.Work, ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.School]
 Geographic_Clustered_types = [ConnectionTypes.Other]
