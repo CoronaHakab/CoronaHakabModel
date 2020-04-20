98ca15f4f7d6b44a3693f7d58da67e8a7fb576a2
Idan Miara <idan@miara.com>
2020-04-20 15:54:37 +0300

    change script default branch

--- linux/aws_setup.sh
+++ linux/aws_setup.sh
@@ -1,4 +1,4 @@
-link_base=https://raw.githubusercontent.com/CoronaHakab/CoronaHakabModel/develop_linux/linux/
+link_base=https://raw.githubusercontent.com/CoronaHakab/CoronaHakabModel/develop/linux/
 file_links=(deb_install_python3.8.sh aws_cli.sh deb_run.sh)
 
 SECONDS=0
