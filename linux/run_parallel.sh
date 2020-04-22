5c8571b53e32281ca6cddbdd7b8038a5d0b9434b
Meir Halachmi <meir.halachmi@gmail.com>
2020-04-22 13:32:09 +0300

    ...

---
+++ linux/run_parallel.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+num_of_runs="$1"
+shift
+
+for ((i=0; i< $num_of_runs - 1; i++))
+do
+   python3.8 ./src/corona_hakab_model/main.py $@ > "${i}_log.tmp" 2>&1 &
+   echo "Running iteration ${i} in the background"
+done
+echo "Running iteration ${i}, it might take a while..."
+for ((i=0; i< $num_of_runs - 1; i++))
+do
+   grep "OUTPUT FOLDER" "${i}_log.tmp"
+   rm -f "${i}_log.tmp"
