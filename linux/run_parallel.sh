541ccb79307c324009f9ff451c5166ef9c6a867d
bgmoshe <barakgross5@gmail.com>
2020-04-22 17:31:42 +0300

    Now really prinitng to sdtout. FOREAL

--- linux/run_parallel.sh
+++ linux/run_parallel.sh
@@ -8,7 +8,7 @@ do
    echo "Running iteration ${i} in the background"
 done
 echo "Running iteration ${i}, it might take a while..."
-python3.8 ./src/corona_hakab_model/main.py $@ 2>&1
+python3.8 ./src/corona_hakab_model/main.py $@
 echo "Finished iteration ${1}"
 echo "Other outputs folders:"
 for ((i=0; i< $num_of_runs - 1; i++))
