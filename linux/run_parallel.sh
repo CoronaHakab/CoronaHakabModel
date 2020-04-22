7920fbf2def81cd8b1a30316055cac3129bfed60
bgmoshe <barakgross5@gmail.com>
2020-04-22 16:51:35 +0300

    Now printing the last command to stdout, also stopped giving date to the result file, instead final_results.csv because folder has date

--- linux/run_parallel.sh
+++ linux/run_parallel.sh
@@ -8,7 +8,7 @@ do
    echo "Running iteration ${i} in the background"
 done
 echo "Running iteration ${i}, it might take a while..."
-python3.8 ./src/corona_hakab_model/main.py $@ 2>&1 | grep "OUTPUT FOLDER"
+python3.8 ./src/corona_hakab_model/main.py $@ 2>&1
 echo "Finished iteration ${1}"
 echo "Other outputs folders:"
 for ((i=0; i< $num_of_runs - 1; i++))
