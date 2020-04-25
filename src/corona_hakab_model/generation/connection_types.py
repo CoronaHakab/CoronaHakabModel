4e875a00f2284f9793e542dc63b442c6b6e0487b
RoyIsland <roy1.island@gmail.com>
2020-04-25 20:52:54 +0300

    added random connection in geo

--- src/corona_hakab_model/generation/connection_types.py
+++ src/corona_hakab_model/generation/connection_types.py
@@ -10,6 +10,7 @@ class ConnectionTypes(IntEnum):
     Family = 3
     Other = 4
 
+
 # used for circles generation
 In_Zone_types = [ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Family]
 Multi_Zone_types = [ConnectionTypes.Work]
@@ -17,7 +18,8 @@ Whole_Population_types = [ConnectionTypes.Other]
 Non_Random_Age_Types = [ConnectionTypes.Family, ConnectionTypes.School]
 Education_Types = [ConnectionTypes.School, ConnectionTypes.Kindergarten]
 
-With_Random_Connections = [ConnectionTypes.Work, ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Other]
+With_Random_Connections = [ConnectionTypes.School, ConnectionTypes.Kindergarten, ConnectionTypes.Other]
+With_Geo_Random_Connections = [ConnectionTypes.Work]
 
 # used for matrices generations
 Connect_To_All_types = [ConnectionTypes.Family]
--- src/corona_hakab_model/infection.py
+++ src/corona_hakab_model/infection.py
@@ -63,7 +63,24 @@ class InfectionManager:
 
     def _infect_random_connections(self) -> Dict[Agent, InfectionInfo]:
         connections = self.manager.num_of_random_connections * self.manager.random_connections_factor
-        probs_not_infected_from_connection = np.ones_like(connections, dtype=float)
+
+        probs_not_infected_from_random_connection = self._get_probs_not_infected_from_random_connection(connections)
+        probs_not_infected_from_geo_random_connection = self._get_probs_not_infected_from_geo_random_connection(connections)
+
+        probs_not_infected_from_connection = (probs_not_infected_from_random_connection + probs_not_infected_from_geo_random_connection) - 1
+
+        not_infected_probs = np.power(probs_not_infected_from_connection, connections)
+        prob_infected_in_any_circle = 1 - not_infected_probs.prod(axis=1)
+        infections = self.manager.susceptible_vector & \
+                     (np.random.random(len(self.manager.agents)) < prob_infected_in_any_circle)
+
+        infected_indices = np.flatnonzero(infections)
+        return {self.manager.agents[agent_index]: self._get_random_infection_info(int(agent_index),
+                                                                                  1 - not_infected_probs[agent_index])
+                for agent_index in infected_indices}
+
+    def _get_probs_not_infected_from_random_connection(self, connections):
+        probs_not_infected_from_random_connection = np.ones_like(connections, dtype=float)
 
         for connection_type in connection_types.With_Random_Connections:
             for circle in self.manager.social_circles_by_connection_type[connection_type]:
@@ -80,18 +97,35 @@ class InfectionManager:
 
                 prob = total_infectious_random_connections / circle.total_random_connections
 
-                probs_not_infected_from_connection[agents_id, connection_type] = \
+                probs_not_infected_from_random_connection[agents_id, connection_type] = \
                     1 - prob * self.manager.random_connections_strength[connection_type]
 
-        not_infected_probs = np.power(probs_not_infected_from_connection, connections)
-        prob_infected_in_any_circle = 1 - not_infected_probs.prod(axis=1)
-        infections = self.manager.susceptible_vector & \
-                     (np.random.random(len(self.manager.agents)) < prob_infected_in_any_circle)
+        return probs_not_infected_from_random_connection
 
-        infected_indices = np.flatnonzero(infections)
-        return {self.manager.agents[agent_index]: self._get_random_infection_info(int(agent_index),
-                                                                                  1 - not_infected_probs[agent_index])
-                for agent_index in infected_indices}
+    def _get_probs_not_infected_from_geo_random_connection(self, connections):
+        probs_not_infected_from_geo_random_connection = np.ones_like(connections, dtype=float)
+
+        for connection_type in connection_types.With_Geo_Random_Connections:
+            for geographic_circle in self.manager.geographic_circles:
+                circles = geographic_circle.connection_type_to_social_circles[connection_type]
+                agents_id = [agent.index for circle in circles for agent in circle.agents]
+
+                if len(agents_id) == 1:
+                    # One-Agent in circles, you can't randomly meet yourself..
+                    continue
+
+                total_infectious_random_connections = np.dot(
+                    self.manager.contagiousness_vector[agents_id],
+                    connections[agents_id, connection_type],
+                )
+
+                circles_total_random_connections = sum([circle.total_random_connections for circle in circles])
+                prob = total_infectious_random_connections / circles_total_random_connections
+
+                probs_not_infected_from_geo_random_connection[agents_id, connection_type] = \
+                    1 - prob * self.manager.random_connections_strength[connection_type]
+
+        return probs_not_infected_from_geo_random_connection
 
     def _get_infection_info(self, agent_index: int, possible_infectors: np.ndarray):
         if not self.manager.consts.backtrack_infection_sources:
@@ -121,9 +155,19 @@ class InfectionManager:
         connection_type = np.random.choice(len(infection_probs), p=infection_probs / np.sum(infection_probs))
 
         # determine infector
-        infectious_circle = \
-            [c for c in self.manager.social_circles_by_agent_index[agent_id] if c.connection_type == connection_type][0]
-        agents_id = [a.index for a in infectious_circle.agents]
+        agents_id = []
+
+        if connection_type in connection_types.With_Random_Connections:
+            infectious_circle = \
+                [circle for circle in self.manager.social_circles_by_agent_index[agent_id]
+                 if circle.connection_type == connection_type][0]
+            agents_id = [agent.index for agent in infectious_circle.agents]
+
+        if connection_type in connection_types.With_Geo_Random_Connections:
+            infectious_circles = \
+                [circle for geo_circle in self.manager.geographic_circles
+                 for circle in geo_circle.connection_type_to_social_circles[connection_type]]
+            agents_id = [agent.index for circle in infectious_circles for agent in circle.agents]
 
         connections = self.manager.num_of_random_connections * self.manager.random_connections_factor
         infectious_agents = self.manager.contagiousness_vector[agents_id] * connections[agents_id, connection_type]
