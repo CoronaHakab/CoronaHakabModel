4459529b24938c7b1c409f6a4beccd75e7f51cc5
RoyIsland <roy1.island@gmail.com>
2020-05-07 11:43:35 +0300

    added connection type to gen and run, added deceased to sick agents export (#369)

--- linux/gen_and_run_parallel.sh
+++ linux/gen_and_run_parallel.sh
@@ -15,11 +15,11 @@ cp $simulation_consts $output_folder
 
 for ((i=0; i< $num_of_runs - 1; i++))
 do
-   python3.8 ./src/corona_hakab_model/main.py simulate --population-data "${output_folder}/population_data.pickle" --matrix-data "${output_folder}/matrix_data.parasymbolic" -s $simulation_consts  --output "${output_folder}/${i}" > "${i}_log.tmp" 2>&1 &
+   python3.8 ./src/corona_hakab_model/main.py simulate --population-data "${output_folder}/population_data.pickle" --matrix-data "${output_folder}/matrix_data.parasymbolic" --connection_data "${output_folder}/connection_data.pickle" -s $simulation_consts  --output "${output_folder}/${i}" > "${i}_log.tmp" 2>&1 &
    echo "Running iteration ${i} in the background"
 done
 echo "Running iteration ${i}, it might take a while..."
-python3.8 ./src/corona_hakab_model/main.py simulate --population-data "${output_folder}/population_data.pickle" --matrix-data "${output_folder}/matrix_data.parasymbolic" -s $simulation_consts --output "${output_folder}/${i}"
+python3.8 ./src/corona_hakab_model/main.py simulate --population-data "${output_folder}/population_data.pickle" --matrix-data "${output_folder}/matrix_data.parasymbolic" --connection_data "${output_folder}/connection_data.pickle" -s $simulation_consts --output "${output_folder}/${i}"
 echo "Finished iteration ${1}"
 echo "Other outputs folders:"
 for ((i=0; i< $num_of_runs - 1; i++))
