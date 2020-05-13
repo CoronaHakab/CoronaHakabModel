4c9bcf74b92ebc8b5561dad13375ee68a251f733
Meir Halachmi <meir.halachmi@gmail.com>
2020-05-13 12:17:24 +0300

    typo fix

--- src/corona_hakab_model/generation/geographic_circle.py
+++ src/corona_hakab_model/generation/geographic_circle.py
@@ -133,7 +133,7 @@ class GeographicCircle(Circle):
             if adult_type_distribution:
                 # get random amount of adults for each circle according to distribution
                 circles_adult_number = np.random.choice(**adult_type_distribution[size], size=amount_of_circles)
-                # devide population according to age
+                # divide population according to age
                 adults = [agent for agent in agents_for_type if agent.age > 18]
                 non_adults = [agent for agent in agents_for_type if agent.age <= 18]
                 # place adults where needed, non adults elsewhere, where circles need to be populated
