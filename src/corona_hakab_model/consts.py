ff0b484cdbcdcc347ac5555c1da761cd60027e51
bgmoshe <barakgross5@gmail.com>
2020-05-13 13:55:56 +0300

    Did a oops

--- src/corona_hakab_model/consts.py
+++ src/corona_hakab_model/consts.py
@@ -220,7 +220,7 @@ class Consts(NamedTuple):
                     lambda agent: agent.medical_state.name == "Recovered"),
             ]),
     ]
-    should_isolate_positive_detected: bool = True
+    should_isolate_positive_detected: bool = False
     step_to_isolate_dist: Callable = dist(1, 3)  # Isolated today, tomorrow or in 2 days
     sick_to_p_obey_isolation: Dict[bool, float] = {
         True: 1.0,  # 100% sick will obey the isolation.
