ee3dde5a2188c1891e864a8b5cc0b726d7ade85f
bgmoshe <barakgross5@gmail.com>
2020-05-16 18:51:02 +0300

    Less operations in loop

--- src/corona_hakab_model/detection_model/healthcare.py
+++ src/corona_hakab_model/detection_model/healthcare.py
@@ -88,13 +88,13 @@ class HealthcareManager:
         for detection_priority in list(test_location.testing_priorities):
             # First test the prioritized candidates
             who_to_test = np.random.permutation(list(test_candidates_inds))
+            if len(who_to_test) > num_of_tests:
+                who_to_test = who_to_test[:num_of_tests]
             for ind, tested_agent in enumerate(who_to_test):
                 # permute the indices so we won't always test the lower indices
                 if detection_priority.is_agent_prioritized(self.manager.agents[tested_agent]):
                     tested.append(test_location.detection_test.test(self.manager.agents[tested_agent]))
                     test_candidates_inds.remove(tested_agent)  # Remove so it won't be tested again
-                if tested_agent == num_of_tests:
-                    break
             num_of_tests -= len(who_to_test)
         return num_of_tests
 
