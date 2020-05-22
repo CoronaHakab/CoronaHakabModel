cf0ac65dbc07fff9fb304a2a9af2318cd4a58a53
bgmoshe <33910305+bgmoshe@users.noreply.github.com>
2020-05-22 13:15:38 +0300

    Apply suggestions from code review

--- src/corona_hakab_model/detection_model/healthcare.py
+++ src/corona_hakab_model/detection_model/healthcare.py
@@ -103,7 +103,7 @@ class HealthcareManager:
             if test_result:
                 self.positive_detected_today.add(agent.index)
                 if self.manager.consecutive_negative_tests[agent.index] > 0:
-                    self.manager.consecutive_negative_tests[agent.index] = 0
+                self.manager.consecutive_negative_tests[agent.index] = 0
             else:
                 self.manager.consecutive_negative_tests[agent.index] += 1
                 # When isolated agent gets negative result, free him NOW!
